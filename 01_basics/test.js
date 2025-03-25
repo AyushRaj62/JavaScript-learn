a69711acc22e69826bf7dcc1c780001b277f1e0b
Ayush Raj <115057423+AyushRaj62@users.noreply.github.com>
2025-03-25 19:43:21 +0000

    made change

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,7 @@
+"use strict";   //treat all js code as newer version
+
+// alert(3 + 3)    -> we are using nodejs not browser, yha bhi avaible hai ye lekin kuch dusr H     
+
 console.log("ayuush")
+
+
